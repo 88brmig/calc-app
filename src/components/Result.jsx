939979fc6075343ffb02ee4c0dfa917a0e87b104
Bryan Mora <brm.988k@gmail.com>
2021-02-04 00:19:47 -0800

    adding files, not completed yet

---
+++ src/components/Result.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Result = (props) => {
+    return (
+        <div className='result'>
+            <span>0</span>
+        </div>
+    )
+}
+export default Result
